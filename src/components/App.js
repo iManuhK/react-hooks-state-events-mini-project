079d01e0912795d1a7858e504eeceb8996aa850b
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-27 14:55:13 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,6 +8,8 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [optionSelected, setSelectedOption] = useState("All")
+const [searchWord, setSearch] = useState('')
   return (
     <div className="App">
       <h2>My tasks</h2>
